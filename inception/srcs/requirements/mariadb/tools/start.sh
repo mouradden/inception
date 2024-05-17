1d7342c410eff4c1021714549f8929c16ff2ba9b
DENGUIR Mourad <74537479+mouradden@users.noreply.github.com>
2024-05-17 09:22:24 +0100

    Add files via upload

--- inception/srcs/requirements/mariadb/tools/start.sh
+++ inception/srcs/requirements/mariadb/tools/start.sh
@@ -1,20 +1,53 @@
 #!/bin/sh
-mkdir -p /run/mysqld
-chown -R mysql:mysql /run/mysqld
-mkdir -p /var/lib/mysql
-chown -R mysql:mysql /var/lib/mysql
+# mkdir -p /run/mysqld
+# chown -R mysql:mysql /run/mysqld
+# mkdir -p /var/lib/mysql
+# chown -R mysql:mysql /var/lib/mysql
 
-sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
+# sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
 # sed -i "s|.*skip-networking.*|skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 
-echo "FLUSH PRIVILEGES;" > db.sql
-echo "CREATE DATABASE db;" >> db.sql
-echo "CREATE USER 'mourad'@'%' IDENTIFIED BY '123';" >> db.sql
-# echo "ALTER USER 'root'@'%' IDENTIFIED BY '123' ;" >> db.sql;
-echo "GRANT ALL PRIVILEGES ON db.* TO 'mourad'@'%';" >> db.sql
-echo "FLUSH PRIVILEGES;" >> db.sql
+# echo "FLUSH PRIVILEGES;" > db.sql
+# echo "CREATE DATABASE db;" >> db.sql
+# echo "CREATE USER 'mourad'@'%' IDENTIFIED BY '123';" >> db.sql
+# # echo "ALTER USER 'root'@'%' IDENTIFIED BY '123' ;" >> db.sql;
+# echo "GRANT ALL PRIVILEGES ON db.* TO 'mourad'@'%';" >> db.sql
+# echo "FLUSH PRIVILEGES;" >> db.sql
 
-mysqld --user=mysql --bootstrap < db.sql
+# mysqld --user=mysql --bootstrap < db.sql
 
-mariadbd --user=mysql
+#!bin/sh
+
+# echo "
+# USE mysql;
+# FLUSH PRIVILEGES;
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '123';
+# CREATE DATABASE db CHARACTER SET utf8 COLLATE utf8_general_ci;
+# CREATE USER 'mourad'@'%' IDENTIFIED by '123';
+# GRANT ALL PRIVILEGES ON db.* TO 'mourad'@'%';
+# FLUSH PRIVILEGES;" > db.sql
+
+# mariadbd --user=mysql --bootstrap < /db.sql
+
+# rm -f /db.sql
+
+# exec "$@"
+# mariadbd --user=mysql --bind-address=0.0.0.0
+
+#!bin/sh
+
+echo "
+USE mysql;
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '123';
+CREATE DATABASE db CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER 'mourad'@'%' IDENTIFIED by '123';
+GRANT ALL PRIVILEGES ON db.* TO 'mourad'@'%';
+FLUSH PRIVILEGES;" > init_db.sql
+
+mariadbd --user=mysql --bootstrap < /init_db.sql
+
+rm -f /init_db.sql
+
+exec "$@"
