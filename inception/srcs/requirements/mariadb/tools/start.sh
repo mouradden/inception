2c8c02cccf4da30d0fbd28ae5643041aaf904bd4
DENGUIR Mourad <74537479+mouradden@users.noreply.github.com>
2024-05-22 09:42:50 +0100

    commit changes for 22nd

--- inception/srcs/requirements/mariadb/tools/start.sh
+++ inception/srcs/requirements/mariadb/tools/start.sh
@@ -4,9 +4,6 @@
 # mkdir -p /var/lib/mysql
 # chown -R mysql:mysql /var/lib/mysql
 
-# sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
-
-# sed -i "s|.*skip-networking.*|skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 
 # echo "FLUSH PRIVILEGES;" > db.sql
 # echo "CREATE DATABASE db;" >> db.sql
@@ -36,18 +33,32 @@
 # mariadbd --user=mysql --bind-address=0.0.0.0
 
 #!bin/sh
+# sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
+# sed -i "s|.*skip-networking.*|skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+
+# USE mysql;
 echo "
 USE mysql;
 FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '123';
-CREATE DATABASE db CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER 'mourad'@'%' IDENTIFIED by '123';
-GRANT ALL PRIVILEGES ON db.* TO 'mourad'@'%';
-FLUSH PRIVILEGES;" > init_db.sql
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWD}';
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED by '${DB_USER_PASSWD}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+FLUSH PRIVILEGES;" > init.sql
+# echo "
+# USE mysql;
+# FLUSH PRIVILEGES;
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '123';
+# CREATE DATABASE IF NOT EXISTS db ;
+# CREATE USER 'mourad'@'%' IDENTIFIED by '123';
+# GRANT ALL PRIVILEGES ON db.* TO 'mourad'@'%';
+# FLUSH PRIVILEGES;" > init.sql
 
-mariadbd --user=mysql --bootstrap < /init_db.sql
 
-rm -f /init_db.sql
+mariadbd --user=mysql --bootstrap < /init.sql
+# mariadbd  < /init.sql
+
+rm -f /init.sql
 
 exec "$@"
