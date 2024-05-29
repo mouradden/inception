51053ffd933cce97b6f19482b107fa64555142b5
DENGUIR Mourad <74537479+mouradden@users.noreply.github.com>
2024-05-29 10:33:38 +0100

    Add files via upload

--- inception/srcs/requirements/mariadb/tools/start.sh
+++ inception/srcs/requirements/mariadb/tools/start.sh
@@ -1,43 +1,7 @@
 #!/bin/sh
-# mkdir -p /run/mysqld
-# chown -R mysql:mysql /run/mysqld
-# mkdir -p /var/lib/mysql
-# chown -R mysql:mysql /var/lib/mysql
 
-
-# echo "FLUSH PRIVILEGES;" > db.sql
-# echo "CREATE DATABASE db;" >> db.sql
-# echo "CREATE USER 'mourad'@'%' IDENTIFIED BY '123';" >> db.sql
-# # echo "ALTER USER 'root'@'%' IDENTIFIED BY '123' ;" >> db.sql;
-# echo "GRANT ALL PRIVILEGES ON db.* TO 'mourad'@'%';" >> db.sql
-# echo "FLUSH PRIVILEGES;" >> db.sql
-
-# mysqld --user=mysql --bootstrap < db.sql
-
-#!bin/sh
-
-# echo "
-# USE mysql;
-# FLUSH PRIVILEGES;
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '123';
-# CREATE DATABASE db CHARACTER SET utf8 COLLATE utf8_general_ci;
-# CREATE USER 'mourad'@'%' IDENTIFIED by '123';
-# GRANT ALL PRIVILEGES ON db.* TO 'mourad'@'%';
-# FLUSH PRIVILEGES;" > db.sql
-
-# mariadbd --user=mysql --bootstrap < /db.sql
-
-# rm -f /db.sql
-
-# exec "$@"
-# mariadbd --user=mysql --bind-address=0.0.0.0
-
-#!bin/sh
-# sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
-
-# sed -i "s|.*skip-networking.*|skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-
-# USE mysql;
+DB_USER_PASSWD=$(cat "$DB_USER_PASSWD_FILE")
+DB_ROOT_PASSWD=$(cat "$DB_ROOT_PASSWD_FILE")
 echo "
 USE mysql;
 FLUSH PRIVILEGES;
@@ -46,18 +10,8 @@ CREATE DATABASE IF NOT EXISTS ${DB_NAME};
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED by '${DB_USER_PASSWD}';
 GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;" > init.sql
-# echo "
-# USE mysql;
-# FLUSH PRIVILEGES;
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '123';
-# CREATE DATABASE IF NOT EXISTS db ;
-# CREATE USER 'mourad'@'%' IDENTIFIED by '123';
-# GRANT ALL PRIVILEGES ON db.* TO 'mourad'@'%';
-# FLUSH PRIVILEGES;" > init.sql
-
 
 mariadbd --user=mysql --bootstrap < /init.sql
-# mariadbd  < /init.sql
 
 rm -f /init.sql
 
--- inception/srcs/requirements/wordpress/tools/start.sh
+++ inception/srcs/requirements/wordpress/tools/start.sh
@@ -8,30 +8,27 @@ chmod +x wp-cli.phar
 
 mv wp-cli.phar /usr/local/bin/wp
 
-
-
-# rm -rf /var/www/html/wp-config-sample.php
-
 if [ ! -f wp-config.php ]; then
 
 echo "Downloading Wordpress files :"
 
-wp core download --allow-root
+wp core download
 
 echo "Setting Up Wordpress Database : "
-# WP_USER_PASSWD=$(cat "$WP_USER_PASSWD_FILE")
-# echo "------------------------------"
-# echo "the password is : ${WP_USER_PASSWD}"
-# echo "------------------------------"
-wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_USER_PASSWD} --dbhost=mariadb --allow-root
+
+WP_USER_PASSWD=$(cat "$WP_USER_PASSWD_FILE")
+WP_ADMIN_PASSWD=$(cat "$WP_ADMIN_PASSWD_FILE")
+DB_USER_PASSWD=$(cat "$DB_USER_PASSWD_FILE")
+
+wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_USER_PASSWD} --dbhost=mariadb
 
 echo "Installing Wordpress : "
 
-wp core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWD} --admin_email=${WP_ADMIN_MAIL} --skip-email --allow-root
+wp core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWD} --admin_email=${WP_ADMIN_MAIL} --skip-email
 
 echo "Creating User : "
 
-wp user create ${WP_USER} ${WP_USER_MAIL} --role=editor --user_pass=${WP_USER_PASSWD} --allow-root
+wp user create ${WP_USER} ${WP_USER_MAIL} --role=editor --user_pass=${WP_USER_PASSWD}
 
 fi
 
